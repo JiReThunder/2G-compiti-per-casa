5dbb318d87fa23577115a43f3612cabc90b6e05d
JiReThunder <jirethunder@gmail.com>
2017-04-22 09:37:26 +0200

    Add files via upload

---
+++ 02g_Ficuciello_signed char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    signed char max=128
+    signed char min=-128
+    cout<<"il minimo:"<<(int)min<endl;
+    cout<<"il massimo:"<<(int)max<endl;
+    cout<<"il minimo -1:"<<min-1<<endl;
+    cout<<"il massimo +1:"<<max+1<<endl;
+    return 0;
+}
