0257eeeab647ec93d48fdfca3fc930412bcd5a46
JiReThunder <jirethunder@gmail.com>
2017-04-22 09:37:07 +0200

    Add files via upload

---
+++ 02g_Ficuciello_signed int.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    signed int max=128
+    signed int min=-128
+    cout<<"il minimo:"<<min<endl;
+    cout<<"il massimo:"<<max<endl;
+    cout<<"il minimo -1:"<<min-1<<endl;
+    cout<<"il massimo +1:"<<max+1<<endl;
+    return 0;
+}
