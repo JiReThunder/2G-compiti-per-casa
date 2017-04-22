901767f0df1e5f698afac22876bb0e40c27d4146
JiReThunder <jirethunder@gmail.com>
2017-04-22 09:37:43 +0200

    Add files via upload

---
+++ 02g_Ficuciello_char.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    char max=127
+    char min=-128
+    cout<<"il minimo:"<<(int)min<endl;
+    cout<<"il massimo:"<<(int)max<endl;
+    cout<<"il minimo -1:"<<min-1<<endl;
+    cout<<"il massimo +1:"<<max+1<<endl;
+    return 0;
+}
